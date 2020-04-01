a88e71306600fa6ec4da1378130600400b1a5802
NisargBakhai <nisargbakhai1999@gmail.com>
2020-04-01 19:47:48 +0530

    QuickFix - in

--- app/src/main/java/com/example/quickfix/Login.java
+++ app/src/main/java/com/example/quickfix/Login.java
@@ -3,6 +3,8 @@ package com.example.quickfix;
 import android.content.Intent;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.text.TextUtils;
@@ -20,6 +22,7 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -98,7 +101,28 @@ public class Login extends AppCompatActivity  {
                 Intent r=new Intent(Login.this,google.class);
                 startActivity(r);
             }});
+        BottomNavigationView bottomNavigationView = findViewById(R.id.b_nav);
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.services:
+                        startActivity(new Intent(getApplicationContext(),FirstPageAfterLogin.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),home.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profile:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
 
 
 
